00cc4103201ba79c5ece3eb25f4137c2ecafc1e0
yerkinyerkin <yerkinzhaksylykov@gmail.com>
2026-02-16 13:24:26 +0500

    lab3

---
+++ Lab3/JavaScriptTutorials/task2.js
@@ -0,0 +1,24 @@
+let hello = 'Hello world!';
+
+let message;
+
+// copy 'Hello world' from hello into message
+message = hello;
+
+// now two variables hold the same data
+alert(hello); // Hello world!
+alert(message); // Hello world!
+
+//task1
+let admin, name; // can declare two variables at once
+
+name = "John";
+
+admin = name;
+
+alert( admin ); // "John"
+
+//task2
+let ourPlanet = "Earth";
+let currentGuest = "Alex";
+
